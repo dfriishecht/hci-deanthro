7f3192341d40af3ba067643c9d2fa33820f50dd5
coxlaurel <coxlaurel@gmail.com>
2025-11-11 12:48:41 -0500

    setting page & basic  save sync

--- src/components/SettingsOne.jsx
+++ src/components/SettingsOne.jsx
@@ -5,39 +5,54 @@ import './styles/Main.css';
 import Slider from '@rc-component/slider';
 import '@rc-component/slider/assets/index.css';
 
-// const marks = {
-//     0: 'None',
-//     4: "Standard"
-// };
-
 
 function SettingsOne() {
 
+    const [sentimentValue, setSentimentValue] = useState(0)
     const [emotiveValue, setEmotiveValue] = useState(5)
+
+    useEffect(() => {
+        chrome.storage.sync.get(["sentimentValue"], (result) => {
+            if (result.sentimentValue !== undefined) {
+                setSentimentValue(result.sentimentValue);
+            }
+        });
+    }, []);
+
+    const handleSentiment = (value) => {
+        setSentimentValue(value); // update React state
+    };
+
+    const handleSave = () => {
+        chrome.storage.sync.set({ sentimentValue });
+    };
 
     return <>
         <div className='sentiment-level-container'>
-            <p>Sentiment Level:</p>
-            <Slider
-            min={0}
-            max={4}
-            // marks={marks}
-            dots={true}
-            trackStyle={{ backgroundColor: "#FF9553" }}
-            dotStyle={{ borderColor: "#FF9553" }}
-            activeDotStyle={{ borderColor: "#FF9553", backgroundColor: "FF9553" }}
-            />
+            <h3>Sentiment Level:</h3>
+            <div className='sentiment-slider'>
+                <Slider
+                    min={0}
+                    max={4}
+                    dots={true}
+                    trackStyle={{ backgroundColor: "#FF9553" }}
+                    dotStyle={{ borderColor: "#FF9553" }}
+                    activeDotStyle={{ borderColor: "#FF9553", backgroundColor: "FF9553" }}
+                    value={sentimentValue}
+                    onChange={handleSentiment}
+                />
+            </div>
             <div className='sentiment-labels'>
                 <p>None</p>
                 <p>Standard</p>
             </div>
         </div>
         <div className='emotive-count-container'>
-            <p>Emotive Responses Remaining:</p>
+            <h3>Emotive Responses Remaining:</h3>
             <p className='emotive-value'>{`${emotiveValue}`}</p>
         </div>
         <div className='output-format-container'>
-            <p>Output Format:</p>
+            <h3>Output Format:</h3>
             <div className='output-format-item'>
                 <input type='checkbox' id='bullet-list'></input>
                 <label for='bullet-list'>Bullet Point List</label>
@@ -46,6 +61,16 @@ function SettingsOne() {
                 <input type='checkbox' id='wiki'></input>
                 <label for='wiki'>Wikipedia-esque</label>
             </div>
+            <div className='output-format-item'>
+                <input type='checkbox' id='technical'></input>
+                <label for='technical'>Technical/professional</label>
+            </div>
+        </div>
+        <div className='footer'>
+            <button className='grey-btn'>Extra Settings</button>
+            <button className='grey-btn' onClick={handleSave}>
+                Save
+            </button>
         </div>
     </>
 }
